5015944621e9f535ba6d872a8739c51fb2dbee63
João Caçote <joao.f23@hotmail.com>
2022-06-28 16:49:58 +0100

    Div file ready

--- divFile.py
+++ divFile.py
@@ -1,6 +1,3 @@
-number1 = input("Enter a number: ")
-number2 = input("Enter another number: ")
-
-div = number1 / number2
-
-print(number1 + "/" + number2 + "=" + div)
+def div(x, y):
+    div = x / y
+    return div
